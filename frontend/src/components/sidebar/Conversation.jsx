ec53f1358281a00c0de2389b9e11571f8fad9ea6
Kundan Prasad Yadav <ny88119@gmail.com>
2024-06-15 02:30:42 +0530

    find error and comment them for now

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,23 +1,18 @@
-// 
+import React from "react";
 
 const Conversation = ({ conversation, lastIdx, emoji }) => {
-  // Check if conversation is undefined before trying to access its properties
-  if (!conversation) {
-    return null; // or some fallback UI
-  }
-
   return (
     <>
       <div className="flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer">
         <div className="avatar online">
           <div className="w-12 rounded-full">
-            <img src={conversation.profilePic} alt="user avatar" />
+            {/* <img src={conversation.profilePic} alt="user avatar" /> */}
           </div>
         </div>
 
         <div className="flex flex-col flex-1">
           <div className="flex gap-3 justify-between">
-            <p className="font-bold text-gray-200">{conversation.fullName}</p>
+            {/* <p className="font-bold text-gray-200">{conversation.fullName}</p> */}
             <span className="text-xl">{emoji}</span>
           </div>
         </div>
